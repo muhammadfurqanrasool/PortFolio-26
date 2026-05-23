c5f9034ba8bac3b327f892408a5e177466582f8e
Muhammad Furqan Rasool <162313059+muhammadfurqanrasool@users.noreply.github.com>
2026-05-23 16:25:06 +0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -41,7 +41,7 @@ const Projects = () => {
                         })}
                     </div>
                     <div className="links">
-                            <Link target="_blank" to={`/${el.githubLink}`}>
+                            <Link target="_blank" to={`${el.githubLink}`}>
                         <div className="github">
                             <div className="icon">
                             </div>
